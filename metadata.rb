818e5d3243ffbf5d1035dbfaec8e9f11f2f64413
rsmitty <rsmitty1025@gmail.com>
2014-03-04 20:16:07 -0500

    one last time

--- metadata.rb
+++
@@ -1,8 +0,0 @@
-name             'devstack'
-maintainer       'Spencer Smith'
-maintainer_email 'rsmitty1025@gmail.com'
-license          'All rights reserved'
-description      'Installs/Configures devstack'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
-depends          'git'
